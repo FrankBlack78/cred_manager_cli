7fe72f74f36088956eaeea085ad601a2733e3bd1
FrankBlack78 <frankblack78@posteo.de>
2022-09-20 19:24:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+def read_requirements():
+    with open('requirements.txt', 'r') as req:
+        content = req.read()
+        requirements = content.split('\n')
+
+    return requirements
 
 
 setup(
     name='cm',
-    version='0.9.0',
+    version='0.1.0',
     author='Frank Black',
     author_email='frankblack78@posteo.de',
+    packages=find_packages(),
+    include_package_data=True,
     py_modules=['main'],
-    install_requires=[
-        'Click',
-    ],
+    install_requires=read_requirements(),
     entry_points="""
         [console_scripts]
         cm=main:cli
